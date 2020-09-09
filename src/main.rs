6e4ec76a7536234e4c3ab12a5861bba02c5f7c82
Griffin Berlstein <griffin@berlste.in>
2020-09-09 01:24:24 -0400

    serializing and deserializing works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod bril;
+
+use std::io::{self, Read};
+use std::process::exit;
+
+use serde_json;
+
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut buffer = String::new();
+    let mut handle = stdin.lock();
+
+    match handle.read_to_string(&mut buffer) {
+        Ok(n) => {}
+        Err(error) => {
+            eprint!("Encountered error {}", error);
+            exit(1)
+        }
+    }
+
+    let v: bril::Program = serde_json::from_str(&buffer).unwrap();
+    print!("{}\n", serde_json::to_string(&v).unwrap());
 }
